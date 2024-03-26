5443c8a34e6c11d913dbbd2cc66fe0f6f7faf019
nnoiid <dionduriqi11@gmail.com>
2024-03-26 23:48:59 +0100

    Create Sign in API

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -26,3 +27,40 @@ export const signup = async (req, res, next) => {
         next(error); 
     }
 };
+
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+
+    if(!email||!password||email===''||password===''){
+        next(errorHandler(400,'All fields are required'));
+    }
+
+    try{
+        const validUser=await User.findOne({where:{email}});
+
+        if(!validUser){
+            next(errorHandler(404,'User not found!'));
+        }
+
+        const validPassword=bcryptjs.compareSync(password,validUser.password);
+
+        if(!validPassword){
+            next(errorHandler(400,'Invalid password!'));
+        }
+
+        const token=jwt.sign(
+            {id:validUser.id}, process.env.JWT_SECRET
+        )
+
+        const {password:pass, ...rest}=validUser.toJSON();
+
+        delete rest.password;
+
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true
+        }).json(rest);
+
+    }catch(error){
+        next(error);
+    }
+}
