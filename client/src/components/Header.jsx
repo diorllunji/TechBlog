8b6ba06d4c0f97537cf02561bdfb5503a2c0a20a
nnoiid <of56182@ubt-uni.net>
2024-03-30 01:48:14 +0100

    Display logged in user data on frontend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,14 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, DropdownDivider, Navbar, TextInput } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon} from 'react-icons/fa'
 import React from 'react'
+import {useSelector} from 'react-redux'
 
 export default function Header() {
     const path=useLocation().pathname;
+    const {currentUser}=useSelector(state=>state.user);
+
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -28,11 +31,28 @@ export default function Header() {
             <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
                 <FaMoon/>
             </Button>
-            <Link to='/sign-in'>
+            {currentUser?(
+                <Dropdown arrowIcon={false} inline label={<Avatar alt='user' img={currentUser.profilePicture}/>}>
+                    <Dropdown.Header>
+                        <span className='block text-sm'>@{currentUser.username}</span>
+                        <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                    </Dropdown.Header>
+                    <Link to={'/dashboard?tab=profile'}>
+                        <Dropdown.Item>Profile</Dropdown.Item>
+                    </Link>
+                    <Dropdown.Divider/>
+                    <Dropdown.Item>Sign Out</Dropdown.Item>
+                </Dropdown>
+            ):
+            (
+                <Link to='/sign-in'>
                 <Button gradientDuoTone='purpleToBlue'>
                     Sign In
                 </Button>
             </Link>
+            )
+        }
+            
             </div>
             <Navbar.Collapse>
                 <Navbar.Link active={path==="/"}>
