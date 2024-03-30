365ec12801cb3766b3dc3e00774d63fad9dfc257
nnoiid <of56182@ubt-uni.net>
2024-03-30 02:06:58 +0100

    Dark Mode and Dashboard Access Customization

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { Avatar, Button, Dropdown, DropdownDivider, Navbar, TextInput } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
+import {FaMoon,FaSun} from 'react-icons/fa'
 import React from 'react'
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
+import { toggleTheme } from '../redux/theme/themeSlice.js';
 
 export default function Header() {
     const path=useLocation().pathname;
-    const {currentUser}=useSelector(state=>state.user);
+    const {currentUser}=useSelector((state)=>state.user);
+    const dispatch=useDispatch();
+    const {theme}=useSelector((state)=>state.theme);
 
   return (
     <Navbar className='border-b-2'>
@@ -28,8 +31,8 @@ export default function Header() {
             <AiOutlineSearch/>
         </Button>
         <div className='flex gap-2 md:order-2'>
-            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-                <FaMoon/>
+            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toggleTheme())}>
+                {theme==='light'?<FaSun/>:<FaMoon/>}
             </Button>
             {currentUser?(
                 <Dropdown arrowIcon={false} inline label={<Avatar alt='user' img={currentUser.profilePicture}/>}>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import userReducer from './user/userSlice.js';
+import themeReducer from './theme/themeSlice.js';
 import { persistReducer } from 'redux-persist';
 import storage from "redux-persist/lib/storage";
 import persistStore from "redux-persist/es/persistStore";
 
 const rootReducer = combineReducers({
     user: userReducer,
+    theme:themeReducer,
 });
 
 const persistConfig = {
