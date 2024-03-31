1a24a5c49d56b745e694fb8bd4867958786d6ca4
nnoiid <of56182@ubt-uni.net>
2024-03-31 01:37:47 +0100

    Added Dashboard Sidebar

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Sidebar from 'flowbite-react'
+import HiUser, { HiArrowSmRight } from 'react-icons/hi';
+import { useEffect,useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function DashSidebar() {
+    const location=useLocation();
+    const [tab,setTab]=useState('');
+    useEffect(()=>{
+      const urlParams=new URLSearchParams(location.search);
+      const tabFromUrl=urlParams.get('tab');
+      if(tabFromUrl){
+        setTab(tabFromUrl);
+      }
+    },[location.search])
+  return (
+    <Sidebar classname='w-full md:w-56'>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to='/dashboard?tab=profile'>
+                <Sidebar.Item active={tab==='profile'} icon={HiUser} label={"User"} labelColor='dark'>
+                    Profile
+                </Sidebar.Item>
+                </Link>
+                <Sidebar.Item icon={HiArrowSmRight} classname='cursor-pointer'>
+                    Sign Out
+                </Sidebar.Item>
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
