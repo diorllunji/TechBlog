d3637b99c053934a53f0f9c17443c6b1ff9cd7a8
of56182 <of56182@ubt-uni.net>
2024-06-05 16:14:36 +0200

    Edit functionality to comments

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -2,10 +2,13 @@ import moment from 'moment';
 import React, { useEffect, useState } from 'react';
 import {FaThumbsUp} from 'react-icons/fa';
 import { useSelector } from 'react-redux';
+import {Button, Textarea} from 'flowbite-react'
 
-function Comment({ comment, onLike }) {
+function Comment({ comment, onLike, onEdit }) {
     const [user, setUser] = useState({});
-    const {currentUser}=useSelector((state)=>state.user)
+    const {currentUser}=useSelector((state)=>state.user);
+    const [isEditing,setIsEditing]=useState(false);
+    const [editedContent,setEditedContent]=useState(comment.content);
 
     useEffect(() => {
         const getUser = async () => {
@@ -22,6 +25,31 @@ function Comment({ comment, onLike }) {
         getUser();
     }, [comment]);
 
+    const handleEdit=()=>{
+        setIsEditing(true);
+        setEditedContent(comment.content);
+    }
+
+    const handleSave=async()=>{
+        try{
+            const res=await fetch(`/api/comment/editComment/${comment._id}`,{
+                method:'PUT',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                    content:editedContent
+                })
+            });
+            if(res.ok){
+                setIsEditing(false);
+                onEdit(comment,editedContent);
+            }
+        }catch(error){
+            console.log(error.message)
+        }
+    }
+
     return (
         <div className="flex p-4 border-b dark:border-gray-600 text-sm">
             <div className="flex-shrink-0 mr-3">
@@ -32,7 +60,34 @@ function Comment({ comment, onLike }) {
                     <span className="font-bold mr-1 text-xs truncate">{user ? `@${user.username}` : 'Anonymous user'}</span>
                     <span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
                 </div>
-                <p className='text-gray-500 pb-2'>{comment.content}</p>
+                {isEditing ?(
+                    <>
+                    <Textarea className='mb-2'
+                    value={editedContent}
+                    onChange={(e)=>setEditedContent(e.target.value)}
+                   />
+                   <div className='flex justify-end gap-2 text-xs'>
+                    <Button
+                    type='button'
+                    size='sm'
+                    gradientDuoTone='purpleToBlue'
+                    onClick={handleSave}
+                    >Save
+                    </Button>
+                    <Button
+                    type='button'
+                    size='sm'
+                    gradientDuoTone='purpleToBlue'
+                    outline
+                    onClick={()=>setIsEditing(false)}
+                    >Cancel
+                    </Button>
+                   </div>
+                   </>
+                    
+                ):(
+                    <>
+                    <p className='text-gray-500 pb-2'>{comment.content}</p>
                 <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
                     <button type='button' onClick={()=>onLike(comment._id)} 
                     className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser.id)&&'!text-blue-500'}`}>
@@ -43,7 +98,21 @@ function Comment({ comment, onLike }) {
                         comment.numberOfLikes>0 && comment.numberOfLikes+" "+(comment.numberOfLikes===1?"like":"likes")
                     }
                     </p>
+                    {
+                        currentUser && (currentUser.id===comment.userId || currentUser.isAdmin)&&(
+                            <button type='button' 
+                            onClick={handleEdit}
+                            className='text-gray-400 hover:text-blue-500'>
+                                Edit
+                            </button>
+                        )
+                    }
                 </div>
+                </>
+                )
+                
+            }
+                
             </div>
         </div>
     );
